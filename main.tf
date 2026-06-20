1ff08fa0fd1c28b38e635805c418180b4bf311f1
jyotshinde <jyotshinde@users.noreply.github.com>
2026-06-20 05:09:35 +0000

    secuity group rules for port 22 and 80

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "aws_ec2_instance" "ec2-demo" {
     ami           = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
     user_data = file("${path.module}/app1-install.sh")
+    security_groups = [aws_security_group.demo-sg.id]
     tags = {
         Name = "my-ec2-instance"
     }
