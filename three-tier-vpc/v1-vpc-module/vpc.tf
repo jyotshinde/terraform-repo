f4fe5bc500d0d10bbe7dbfb6c24b411c26da2de0
jyotshinde <jyotshinde@users.noreply.github.com>
2026-06-24 10:32:56 +0000

    vpc validation is success

--- three-tier-vpc/v1-vpc-module/vpc.tf
+++ three-tier-vpc/v1-vpc-module/vpc.tf
@@ -1,7 +1,28 @@
 module "vpc_example_complete" {
-  source  = "terraform-aws-modules/vpc/aws//examples/complete"
+  source  = "terraform-aws-modules/vpc/aws"
   version = "6.6.1"
 
+  #basic details
   name = "full-vpc"
   cidr = "10.0.0.0/16"
+  #subnets
+  azs                 = ["ap-southeast-1a", "ap-southeast-1b"]  
+  public_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets     = ["10.0.5.0/24", "10.0.6.0/24"]
+  database_subnets    = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  create_database_subnet_group = true
+  create_database_subnet_route_table = true
+
+  #NAT Gateway
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  tags = {
+    owner       = "thejyot"
+    Environment = "dev"
+  }
+  
 }
